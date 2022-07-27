bfb910364c9f569009468d9cbd8c3e995f451410
igoorsimoess <igor.s.simoes@gmail.com>
2022-07-27 00:04:13 -0300

    Requisições web com ruby

---
+++ Web/ruby_net.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+
+example = Net::HTTP.get('example.com', '/index.html')
+
+File.open('example.html', 'w') do |line|
+    line.puts(example)
+end
+
+
+# https
+
+https = Net::HTTP.new('reqres.in', 443)
+https.use_ssl = true
+
+response = https.get('/api/users')
+
+puts response.code
+puts response.message
+puts response.body
